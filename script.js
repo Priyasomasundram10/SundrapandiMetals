5ba4c1dc504af61ce3ed8814c765e462cafd74f8
Priyasomasundram10 <68894799+Priyasomasundram10@users.noreply.github.com>
2025-04-16 14:24:04 +1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Animation for the dancing metal characters
+document.querySelector('.metal-character').addEventListener('click', function() {
+    alert('You clicked a dancing metal character!');
+});
